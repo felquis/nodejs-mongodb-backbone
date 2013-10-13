87c7635fe53dd59567bbb92a83ce709a9f9b4ff3
Felquis Gimenes <frgformenton@gmail.com>
2013-10-13 00:17:26 -0300

    Add example 3-1

--- app.js
+++ app.js
@@ -6,26 +6,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/stooges/:name?', function (req, res, next) {
-	var name = req.params.name;
-
-	switch (name ? name.toLowerCase() : '') {
-		case 'larry':
-		case 'curly':
-		case 'moe':
-			res.send(name + ' is my favorite stooge.');
-			break;
-		default:
-			next();
-	}
-});
-
-app.get('/stooges/*?', function (req, res) {
-	res.send('no stooges listed');
+app.configure(function () {
+	app.set('view engine', 'jade');
+	app.set('view options', { layout: true });
+	app.set('views', __dirname + '/views');
+	app.use(express.static(__dirname + '/'));
 });
 
 app.get('/?', function (req, res) {
-	res.send('hello world');
+	res.render('index');
 });
 
 var port = 8080;
